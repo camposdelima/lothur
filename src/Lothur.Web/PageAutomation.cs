71f4ce1c98b2051da5373aa87ec0271575d0dc27
camposdelima <cristiano.campos@outlook.com>
2017-08-04 01:31:06 -0300

    Criação de Lothur.Web;

---
+++ src/Lothur.Web/PageAutomation.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+
+namespace Lothur.Web
+{
+    public interface IPageAutomation:IWebAutomation
+    {
+    }
+
+    public abstract class PageAutomation: WebAutomation, IPageAutomation
+    {
+        protected string PageAddress { get; }
+
+        public PageAutomation(string pageAddress, System.Uri uri,ICapabilities capabilities):base(uri, capabilities)
+        {
+            this.PageAddress = pageAddress;
+            Navigate();
+        }
+
+        protected virtual void Navigate()
+        {
+            this.Navigate(this.PageAddress);
+        }
+
+    }
+}
