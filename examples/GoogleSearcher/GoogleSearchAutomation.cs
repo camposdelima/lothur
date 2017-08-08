a85189e531671f4320a39ad9c8b40d90bd1cd484
camposdelima <cristiano.campos@outlook.com>
2017-08-08 04:05:51 -0300

    Creation of GoogleSearcher example.

---
+++ examples/GoogleSearcher/GoogleSearchAutomation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace GoogleSearcher
+{
+    public class GoogleSearchAutomation: Lothur.Web.PageAutomation
+    {
+        private const string ADDRESS = "http://google.com.br";
+
+        public GoogleSearchAutomation(System.Uri driverServerURI,ICapabilities capabilities):base(ADDRESS, driverServerURI, capabilities)
+        {}
+
+        public override void Execute()
+        {
+            this.Find("#lst-ib").SendKeys("hello world");
+            this.Find("input[name='btnK']").Submit();
+        }
+    }
+}
---
+++ examples/GoogleSearcher/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace GoogleSearcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (GoogleSearchAutomation search = CreateSearchAutomation())
+            {
+                search.Execute();
+                Console.WriteLine("Done!");
+            }
+        }
+
+
+        private static GoogleSearchAutomation CreateSearchAutomation()
+        {
+            Uri driverServerURI = new Uri("http://192.168.1.103:4444");
+            ICapabilities driverCapabilities = new DesiredCapabilities();
+
+            return new GoogleSearchAutomation(driverServerURI, driverCapabilities);
+        }
+    }
+}
