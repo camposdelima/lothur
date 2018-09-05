645a7c7470b9429f869d6bc64ea44d444993b8fb
RLuna <rodrigobraga26@gmail.com>
2018-09-04 23:47:01 -0300

    update version selenium

---
+++ src/Lothur.Web/MiddlewareAutomation.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System.Threading.Tasks;
+
+namespace Lothur.Web
+{
+    public interface IMiddlewareAutomation : IWebAutomation<Task>
+    {
+    }
+
+    public abstract class MiddlewareAutomation : WebAutomation<Task>, IMiddlewareAutomation
+    {
+
+        public MiddlewareAutomation(IWebDriver driver) : base(driver)
+        {
+        }
+    }
+}
